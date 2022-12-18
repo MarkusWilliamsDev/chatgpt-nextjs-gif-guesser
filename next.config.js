7645fc4985b4cf048ddb155e492b013ee60eb867
MarkusWilliams <markuswilliamsdev@gmail.com>
2022-12-17 23:35:44 -0500

    trying new config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+};
+module.exports = {
+	webpack: (config, { isServer }) => {
+		// Fixes npm packages that depend on `fs` module
+		if (!isServer) {
+			config.node = {
+				fs: "empty",
+			};
+		}
 
-module.exports = nextConfig
+		return config;
+	},
+};
+
+module.exports = nextConfig;
